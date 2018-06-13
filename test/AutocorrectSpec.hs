f3e7a6f23299ff9f4df701e258e6bf9673ba6c07
Vojtech Mraz <mraz.vojta@gmail.com>
2018-06-13 13:31:58 +0200

    First set of test for autocorrect
    Tests for first order mistakes separatelly
    General tests for second order mistakes

---
+++ test/AutocorrectSpec.hs
@@ -0,0 +1,42 @@
+module AutocorrectSpec (spec) where
+
+import Test.Hspec    
+
+import Autocorrect
+    
+spec :: Spec
+spec = do
+    describe "Mistakes of the first order" $ do
+        it "with delete edit" $ do
+            spell "hose" `shouldBe` "house"
+            spell "corect" `shouldBe` "correct"
+            spell "peson" `shouldBe` "person"
+            spell "economc" `shouldBe` "economic"
+            spell "stuborn" `shouldBe` "stubborn"
+        it "with transpose edit" $ do
+            spell "huose" `shouldBe` "house"
+            spell "corretc" `shouldBe` "correct"
+            spell "pesron" `shouldBe` "person"
+            spell "eocnomic" `shouldBe` "economic"
+            spell "stubbron" `shouldBe` "stubborn"
+        it "with replace edit" $ do
+            spell "houst" `shouldBe` "house"
+            spell "cprrect" `shouldBe` "correct"
+            spell "perdon" `shouldBe` "person"
+            spell "economoc" `shouldBe` "economic"
+            spell "stuvborn" `shouldBe` "stubborn"
+        it "with insert edit" $ do
+            spell "houyse" `shouldBe` "house"
+            spell "correcrt" `shouldBe` "correct"
+            spell "persone" `shouldBe` "person"
+            spell "erconomic" `shouldBe` "economic"
+            spell "sztubbborn" `shouldBe` "stubborn"
+    describe "Mistakes of the second order" $ do
+        it "general combination of two mistakes" $ do
+            spell "bycicle" `shouldBe` "bicycle"
+            spell "photograf" `shouldBe` "photograph"
+            spell "petsonlity" `shouldBe` "personality"
+            spell "paaperwokr" `shouldBe` "paperwork"
+            spell "permissont" `shouldBe` "permission"
+            spell "replacamen" `shouldBe` "replacement"
+            
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,10 @@
+import Test.Hspec
+
+import qualified AutocorrectSpec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Autocorrect module"      AutocorrectSpec.spec
