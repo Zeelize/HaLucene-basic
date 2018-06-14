7f3bcddbf4d5e7d9229741b36b7010f2460e617b
Vojtech Mraz <mraz.vojta@gmail.com>
2018-06-14 18:09:17 +0200

    Reading terms from the index file and calculating relevancy

--- src/IndexSearcher.hs
+++ src/IndexSearcher.hs
@@ -41,3 +41,48 @@ EDIT #1: Because we are storing only term per document and not their frequencies
 That is possible improvement, to add TF into IndexWriter.
 -}
 module IndexSearcher where
+
+import Data.List
+import System.IO.Unsafe
+
+import qualified Data.Text as T
+import qualified Data.Map as M
+import qualified Data.Set as S
+import qualified StandardAnalyzer as SA
+
+-- | Map of documents and number fo terms relevant for it
+type TermsDocsMap = M.Map Int Int
+
+-- | Find top N relevant documents for the user query and indexes on given path
+findRelevantDocs :: Int -> String -> FilePath -> FilePath -> [T.Text]
+findRelevantDocs n query ifp dfp = do
+    -- analyze user query
+    let analQ = SA.analyze (T.pack query)
+    let relDoc = preSearch analQ ifp
+    -- sort map and return top N 
+    []
+
+-- | Pre-proccess for index searcher
+preSearch :: T.Text -> FilePath -> TermsDocsMap
+preSearch q ifp = searcher qterms ti M.empty
+    where 
+        ti = T.lines . T.pack . unsafePerformIO $ readFile ifp
+        qterms = S.fromList . nub . T.words $ q
+
+-- | For all terms it will compute final document relevancy number 
+searcher :: S.Set T.Text -> [T.Text] -> TermsDocsMap -> TermsDocsMap
+searcher _ [] tdm = tdm
+searcher q (x:xs) tdm 
+    | S.null q = tdm
+    | S.member term q = searcher nq xs ntdm
+    | otherwise = searcher q xs tdm
+    where
+        splitted = T.splitOn (T.pack "-") x 
+        term = head splitted 
+        docs = head . tail $ splitted
+        nq = S.delete term q
+        ntdm = proccessDocLine tdm (T.splitOn (T.pack ";") docs)
+        -- It will proccess line with docs and add them to do 
+        proccessDocLine :: TermsDocsMap -> [T.Text] -> TermsDocsMap
+        proccessDocLine tdm line = tdm
+    
