6f6b8a4f262e6efa935a010537fccd98a1be0e28
Vojtech Mraz <mraz.vojta@gmail.com>
2018-06-15 11:24:45 +0200

    IndexSearcher modified to work with term frequencies also

--- src/IndexSearcher.hs
+++ src/IndexSearcher.hs
@@ -12,33 +12,30 @@ Its possible to lookup the terms and user query in the inverted indexes created 
 Basic theory behind:
 Lets say we have inverted indexes:
     Terms:
-        hello-1;2;
-        welcome-1;
-        red-1;
-        dwarf-1;
-        world-2;
+        hello-1,2;2,1;
+        welcome-1,1;
+        red-1,2;
+        dwarf-1,1;
+        world-2,1;
     Docs:
         1-red dwarf,5;
         2-hello word,2;
 
 And user query is [hello], it will find both of the document, but it will return document number 2 like more relevant, 
-because its lenght is smaller than size of the first document (If we will be someday store TF (term frequency) also,
-we can compute it easily with the equation TF/DF, it means number of occcurences).
+because its lenght is smaller than size of the first document (we can compute it easily with the equation TF/DF, 
+it means number of occcurences).
 
 If we have longer user query, it will compute for every single term separately and after that it will sum the output.
 Lets say we have user query [Hello red]. 
 Relevancy for term [hello]:
-    doc1 - 1/5
+    doc1 - 2/5
     doc2 - 1/2
 Relevancy for term [red]:
-    doc1 - 1/5
+    doc1 - 2/5
     doc2 - 0/2
 Final relevancies:
-    doc1 = 1/5 + 1/5
+    doc1 = 2/5 + 2/5
     doc2 = 1/2
-
-EDIT #1: Because we are storing only term per document and not their frequencies, index searcher can do some mistakes. 
-That is possible improvement, to add TF into IndexWriter.
 -}
 module IndexSearcher where
 
@@ -90,10 +87,12 @@ searcher q (x:xs) tdm
         proccessTermLine tdm [] = tdm
         proccessTermLine tdm (x:xs) = proccessTermLine ntdm xs
             where
-                docId = read $ T.unpack x :: Int
+                split = T.splitOn (T.pack ",") x                
+                docId = read $ T.unpack (head split) :: Int
+                termFreq = read $ T.unpack (head . tail $ split) :: Int
                 ntdm = case M.member docId tdm of
-                    True -> M.adjust (+ 1) docId tdm 
-                    _ -> M.insert docId 1 tdm
+                    True -> M.adjust (+ termFreq) docId tdm 
+                    _ -> M.insert docId termFreq tdm
 
 -- | Merge relevant documents according to user query with docs index
 mergeDocs :: FilePath -> TermsDocsMap -> [(Double, T.Text)]
